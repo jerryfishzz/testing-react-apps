1044be366bb88fa87ecaa39330f8f7f8259a95b9
Jerry Zhang <jerryfish@163.com>
2022-08-27 19:55:05 +1200

    Complete ex1
    feat: complete ex1

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -11,24 +11,57 @@ import Counter from '../../components/counter'
 // Luckily, it's handled for you by React Testing Library :)
 global.IS_REACT_ACT_ENVIRONMENT = true
 
+// This is from the tutorial to replace div.remove() at the end of the test.
+// It can solve the problem that div.remove() cannot be performed if errors have been thrown.
+// This is a temp solution. Will have better solution soon.
+beforeEach(() => {
+  document.body.innerHTML = ''
+})
+
 test('counter increments and decrements when the buttons are clicked', () => {
   // 🐨 create a div to render your component to (💰 document.createElement)
+  const div = document.createElement('div')
+
   //
   // 🐨 append the div to document.body (💰 document.body.append)
+  document.body.append(div)
+
   //
   // 🐨 use createRoot to render the <Counter /> to the div
+  const root = createRoot(div)
+  act(() => root.render(<Counter />))
+
+  console.log(document.body.innerHTML)
+
   // 🐨 get a reference to the increment and decrement buttons:
   //   💰 div.querySelectorAll('button')
+  const [decrement, increment] = div.querySelectorAll('button')
+
   // 🐨 get a reference to the message div:
   //   💰 div.firstChild.querySelector('div')
+  const message = div.firstChild.querySelector('div')
+
   //
   // 🐨 expect the message.textContent toBe 'Current count: 0'
+  expect(message.textContent).toBe('Current count: 0')
+
   // 🐨 click the increment button (💰 act(() => increment.click()))
+  act(() => increment.click())
+
   // 🐨 assert the message.textContent
+  expect(message.textContent).toBe('Current count: 1')
+
   // 🐨 click the decrement button (💰 act(() => decrement.click()))
+  act(() => decrement.click())
+
   // 🐨 assert the message.textContent
+  expect(message.textContent).toBe('Current count: 0')
+
   //
   // 🐨 cleanup by removing the div from the page (💰 div.remove())
+  // div.remove() Note, this will not run if the code above throw error already
+  // console.log(document.body.innerHTML)
+
   // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
 })
 

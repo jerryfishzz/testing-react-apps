66a40f71837457a3d73bfd3d4fbca84d047e1693
Jerry Zhang <jerryfish@163.com>
2022-08-31 14:09:39 +1200

    Partially complete ex 4
    feat: partially complete ex 4

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -4,24 +4,55 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+// import { faker } from '@faker-js/faker';
 import Login from '../../components/login'
+import { build, fake } from '@jackfranklin/test-data-bot';
 
-test('submitting the form calls onSubmit with username and password', () => {
+const loginBuilder = build('Login', {
+  fields: {
+    username: fake(f => f.name.findName()),
+    password: fake(f => f.internet.password())
+  }
+})
+
+test('submitting the form calls onSubmit with username and password', async () => {
   // 🐨 create a variable called "submittedData" and a handleSubmit function that
   // accepts the data and assigns submittedData to the data that was submitted
   // 💰 if you need a hand, here's what the handleSubmit function should do:
   // const handleSubmit = data => (submittedData = data)
+  let submittedData
+  const handleSubmit = data => (submittedData = data)
+
   //
   // 🐨 render the login with your handleSubmit function as the onSubmit prop
+  render(<Login onSubmit={handleSubmit} />)
+  screen.debug()
+
   //
   // 🐨 get the username and password fields via `getByLabelText`
+  const usernameInput = screen.getByLabelText(/username/i)
+  const passwordInput = screen.getByLabelText(/password/i)
+
+  const {username, password} = loginBuilder({
+    overrides: {
+      password: 'abc'
+    }
+  })
+
   // 🐨 use `await userEvent.type...` to change the username and password fields to
   //    whatever you want
+  await userEvent.type(usernameInput, username)
+  await userEvent.type(passwordInput, password)
+
   //
   // 🐨 click on the button with the text "Submit"
+  const button = screen.getByRole('button', {name: /submit/i})
+  await userEvent.click(button)
+
   //
   // assert that submittedData is correct
   // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+  expect(submittedData).toEqual({username: username, password: password})
 })
 
 /*

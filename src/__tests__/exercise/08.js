a56523f65ffff4179135b3b8e324a9bc9ba6c8aa
Jerry Zhang <jerryfish@163.com>
2022-11-30 11:01:00 +1300

    Complete 08
    feat: Complete 08

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -12,11 +12,37 @@ import useCounter from '../../components/use-counter'
 // 💰 here's how to use the hook:
 // const {count, increment, decrement} = useCounter()
 
-test('exposes the count and increment/decrement functions', () => {
+function Counter() {
+  const {count, increment, decrement} = useCounter()
+
+  return (
+    <>
+      <p>{`Current count: ${count}`}</p>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>increment</button>
+    </>
+  )
+}
+
+test('exposes the count and increment/decrement functions', async () => {
   // 🐨 render the component
+  render(<Counter />)
+
   // 🐨 get the elements you need using screen
+  const text = screen.getByText(/current count/i)
+  const decrement = screen.getByRole('button', {name: /decrement/i})
+  const increment = screen.getByRole('button', {name: /increment/i})
+
   // 🐨 assert on the initial state of the hook
+  expect(text).toHaveTextContent('Current count: 0')
+
   // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  await userEvent.click(decrement)
+  expect(text).toHaveTextContent('Current count: -1')
+
+  await userEvent.click(increment)
+  await userEvent.click(increment)
+  expect(text).toHaveTextContent('Current count: 1')
 })
 
 /* eslint no-unused-vars:0 */
